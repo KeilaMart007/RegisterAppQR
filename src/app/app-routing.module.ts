d76d2d02dea14aa7fc09c9526fe0d64fae488036
bryan-alexis <br.trecanao@duocuc.cl>
2024-01-07 20:56:03 -0300

    login y cargando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'cargando',
     pathMatch: 'full'
   },
   {
@@ -13,64 +13,65 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'registrar',
-    loadChildren: () => import('./auth/registrar/registrar.module').then( m => m.RegistrarPageModule)
+    loadChildren: () => import('./pages/auth/registrar/registrar.module').then( m => m.RegistrarPageModule)
   },
   {
     path: 'resetpassword',
-    loadChildren: () => import('./auth/resetpassword/resetpassword.module').then( m => m.ResetpasswordPageModule)
+    loadChildren: () => import('./pages/auth/resetpassword/resetpassword.module').then( m => m.ResetpasswordPageModule)
   },
   {
     path: 'homealumno',
-    loadChildren: () => import('./home/homealumno/homealumno.module').then( m => m.HomealumnoPageModule)
+    loadChildren: () => import('./pages/home/homealumno/homealumno.module').then( m => m.HomealumnoPageModule)
   },
   {
     path: 'homeprofesor',
-    loadChildren: () => import('./home/homeprofesor/homeprofesor.module').then( m => m.HomeprofesorPageModule)
+    loadChildren: () => import('./pages/home/homeprofesor/homeprofesor.module').then( m => m.HomeprofesorPageModule)
   },
   {
     path: 'cargando',
-    loadChildren: () => import('./cargando/cargando.module').then( m => m.CargandoPageModule)
+    loadChildren: () => import('./pages/cargando/cargando.module').then( m => m.CargandoPageModule)
   },
   {
     path: 'perfilalumno',
-    loadChildren: () => import('./perfil/perfilalumno/perfilalumno.module').then( m => m.PerfilalumnoPageModule)
+    loadChildren: () => import('./pages/perfil/perfilalumno/perfilalumno.module').then( m => m.PerfilalumnoPageModule)
   },
   {
     path: 'perfilprofesor',
-    loadChildren: () => import('./perfil/perfilprofesor/perfilprofesor.module').then( m => m.PerfilprofesorPageModule)
+    loadChildren: () => import('./pages/perfil/perfilprofesor/perfilprofesor.module').then( m => m.PerfilprofesorPageModule)
   },
   {
     path: 'leerqr',
-    loadChildren: () => import('./qr/leerqr/leerqr.module').then( m => m.LeerqrPageModule)
+    loadChildren: () => import('./pages/qr/leerqr/leerqr.module').then( m => m.LeerqrPageModule)
   },
   {
     path: 'mostrarqr',
-    loadChildren: () => import('./qr/mostrarqr/mostrarqr.module').then( m => m.MostrarqrPageModule)
+    loadChildren: () => import('./pages/qr/mostrarqr/mostrarqr.module').then( m => m.MostrarqrPageModule)
   },
   {
     path: 'listaprofesor',
-    loadChildren: () => import('./lista/listaprofesor/listaprofesor.module').then( m => m.ListaprofesorPageModule)
+    loadChildren: () => import('./pages/lista/listaprofesor/listaprofesor.module').then( m => m.ListaprofesorPageModule)
   },
   {
     path: 'listalumno',
-    loadChildren: () => import('./lista/listalumno/listalumno.module').then( m => m.ListalumnoPageModule)
+    loadChildren: () => import('./pages/lista/listalumno/listalumno.module').then( m => m.ListalumnoPageModule)
   },
   {
     path: 'clasesprofesor',
-    loadChildren: () => import('./clases/clasesprofesor/clasesprofesor.module').then( m => m.ClasesprofesorPageModule)
+    loadChildren: () => import('./pages/clases/clasesprofesor/clasesprofesor.module').then( m => m.ClasesprofesorPageModule)
   },
   {
     path: 'clasesalumno',
-    loadChildren: () => import('./clases/clasesalumno/clasesalumno.module').then( m => m.ClasesalumnoPageModule)
+    loadChildren: () => import('./pages/clases/clasesalumno/clasesalumno.module').then( m => m.ClasesalumnoPageModule)
   },
   {
     path: 'horario',
-    loadChildren: () => import('./clases/horario/horario.module').then( m => m.HorarioPageModule)
+    loadChildren: () => import('./pages/clases/horario/horario.module').then( m => m.HorarioPageModule)
   }
+
 ];
 
 @NgModule({
