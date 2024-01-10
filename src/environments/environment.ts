15099b5b33a5b842a5b8e22f2e0b3612d2fee78d
bryan-alexis <br.trecanao@duocuc.cl>
2024-01-10 14:22:26 -0300

    conexion firebase y login creados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBEyOl8Fzctpz9HPVC6CS95BcJfJZ_zdzk",
+    authDomain: "duoc-qr.firebaseapp.com",
+    projectId: "duoc-qr",
+    storageBucket: "duoc-qr.appspot.com",
+    messagingSenderId: "348940376253",
+    appId: "1:348940376253:web:bc244132070a3325cfbfd4",
+    measurementId: "G-5FFCKJVBZP"
+  }
 };
 
 /*
